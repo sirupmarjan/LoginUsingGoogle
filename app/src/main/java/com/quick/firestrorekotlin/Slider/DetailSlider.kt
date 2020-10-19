0d85a94868e7e6c9e26eea0b6e218b2efad3a786
Violin <rizki_violin_radhiyan@git.com>
2020-10-19 13:02:18 +0700

    Add bottom sheet feature

---
+++ app/src/main/java/com/quick/firestrorekotlin/Slider/DetailSlider.kt
@@ -0,0 +1,11 @@
+package com.quick.firestrorekotlin.Slider
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailSlider : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_slider)
+    }
+}
