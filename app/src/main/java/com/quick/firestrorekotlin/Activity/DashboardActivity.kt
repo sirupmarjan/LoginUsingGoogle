a280a7e32e624de3d0c32b6ffa7094e4758147fd
Violin <rizki_violin_radhiyan@git.com>
2020-10-19 15:16:16 +0700

    Add another annoying

--- app/src/main/java/com/quick/firestrorekotlin/Activity/DashboardActivity.kt
+++ app/src/main/java/com/quick/firestrorekotlin/Activity/DashboardActivity.kt
@@ -3,26 +3,57 @@ package com.quick.firestrorekotlin.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.firebase.auth.FirebaseAuth
 import com.quick.firestrorekotlin.R
 import kotlinx.android.synthetic.main.activity_dashboard.*
+import kotlinx.android.synthetic.main.activity_detail_slider.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class DashboardActivity : AppCompatActivity() {
     private lateinit var auth : FirebaseAuth
+    private lateinit var bottomSheetBehavior : BottomSheetBehavior<ConstraintLayout>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
         auth = FirebaseAuth.getInstance()
         var currentUser = auth?.currentUser
 
-        tv_userId.text = currentUser?.uid
-        tv_userName.text = currentUser?.displayName
-        tv_userEmail.text = currentUser?.email
+        bottomSheetBehavior = BottomSheetBehavior.from(btmSheet)
+
+        tv_uId.text = currentUser?.uid
+        tv_uName.text = currentUser?.displayName
+        tv_uEmail.text = currentUser?.email
+        tv_welcome.text = "Selamat Datang "+currentUser?.displayName+" Selamat menjelajahi Dunia baru bersama kami"
 
         btn_logout.setOnClickListener {
-            auth.signOut()
-            startActivity(Intent(this@DashboardActivity, SignInActivity::class.java))
-            finish()
+            CoroutineScope(Dispatchers.IO).launch {
+                try {
+                    auth.signOut().also {
+                        startActivity(Intent(this@DashboardActivity, SignInActivity::class.java))
+                        finish()
+                    }
+                }catch (e:Exception){
+                    withContext(Dispatchers.Main){
+                        Toast.makeText(this@DashboardActivity, e.message, Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+
+        ln_toogle.setOnClickListener {
+            val state =
+                if (bottomSheetBehavior.state == BottomSheetBehavior.STATE_EXPANDED)
+                    BottomSheetBehavior.STATE_COLLAPSED
+                else
+                    BottomSheetBehavior.STATE_EXPANDED
+                bottomSheetBehavior.state = state
+
         }
     }
 }
--- app/src/main/java/com/quick/firestrorekotlin/Slider/DetailSlider.kt
+++ app/src/main/java/com/quick/firestrorekotlin/Slider/DetailSlider.kt
@@ -2,10 +2,21 @@ package com.quick.firestrorekotlin.Slider
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.quick.firestrorekotlin.R
+import kotlinx.android.synthetic.main.activity_dashboard.*
+import kotlinx.android.synthetic.main.activity_detail_slider.*
 
 class DetailSlider : AppCompatActivity() {
+    private lateinit var auth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_slider)
+        auth = FirebaseAuth.getInstance()
+        var currentUser = auth?.currentUser
+
+        tv_uId.text = currentUser?.uid
+        tv_uName.text = currentUser?.displayName
+        tv_uEmail.text = currentUser?.email
     }
 }
